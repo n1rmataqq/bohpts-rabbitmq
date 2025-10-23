8aa5d95dd55f60ca069d33b68661a0fc6795cc02
n1rmata <kiryl.valiushka@gmail.com>
2025-10-23 13:40:31 +0200

    refactor ClanStatV1 to include nested ClanStat record

--- src/main/java/com/bohpts/messaging/payload/stat/ClanStatV1.java
+++ src/main/java/com/bohpts/messaging/payload/stat/ClanStatV1.java
@@ -3,11 +3,20 @@ package com.bohpts.messaging.payload.stat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ClanStatV1(
-        Long clanId,
-        Integer members,
-        Integer onlineMembers
+        List<ClanStat> clanStats
 ) {
+
+    @Builder
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record ClanStat(
+            Long clanId,
+            Integer members,
+            Integer onlineMembers
+    ) {
+    }
 }
